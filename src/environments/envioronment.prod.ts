8b2cf81d041b29aa50904e4a3f6bef8066e835bb
Asdex1240 <jose.morenobrn@uanl.edu.mx>
2022-06-20 12:53:46 -0500

    Mejoras para la aplicacion en produccion

---
+++ src/environments/envioronment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: true,
+    firebaseConfig:{
+        apiKey: "AIzaSyB90__xOxb5so36PPOPQ6qWqzzJ4CLuRa8",
+        authDomain: "checkyourmoney-3c563.firebaseapp.com",
+        projectId: "checkyourmoney-3c563",
+        storageBucket: "checkyourmoney-3c563.appspot.com",
+        messagingSenderId: "405678366598",
+        appId: "1:405678366598:web:0f0c9aadb5fa2f0f5c9607",
+        measurementId: "G-GZ74GQMHM1"
+      }
+  };
